e485f856f8df8005edcbe31690218d2ef04d7fe4
Mindaugas Petronis <63074962+dzastsed@users.noreply.github.com>
2024-05-04 23:37:04 +0300

    2024-05-04 update
    separated ui and main code, fixed file or folder selection bug

--- main.py
+++ main.py
@@ -1,62 +1,44 @@
 import os
-from tkinter import filedialog, Menu, messagebox
-from tkinter import *
-from tkinter.ttk import *
 from binary_reader import BinaryReader
-from playsound import playsound
-
-
-def close():
-    exit()
-
-
-def play():
-    playsound('Electrodynamix.mp3')
-
+import ui
+from tkinter import filedialog, messagebox
 
 
 class File:
-    def __init__(self):
+    def __init__(self, ui_instance):
         self.input_ps3 = []
         self.input_pc = []
         self.output_ps3 = []
         self.output_pc = []
+        self.ui = ui_instance
 
     def choose_ps3_input(self):
         file = filedialog.askopenfilename()
-        if file == "":
-            return
-        else:
+        if file:
             self.input_ps3.append(file)
-            lbl1.configure(text="Chosen save file: " + file)
-            print (f"DEBUG: chosen PS3 savefile: {self.input_ps3}")
+            self.ui.lbl1.configure(text="Chosen save file: " + file)
+            print(f"DEBUG: chosen PS3 savefile: {file}")
 
     def choose_pc_input(self):
         file_pc = filedialog.askopenfilename()
-        if file_pc == "":
-            return
-        else:
+        if file_pc:
             self.input_pc.append(file_pc)
-            lbl3.configure(text="Chosen save file: " + file_pc)
-            print (f"DEBUG: chosen PC savefile: {self.input_pc}")
+            self.ui.lbl3.configure(text="Chosen save file: " + file_pc)
+            print(f"DEBUG: chosen PC savefile: {file_pc}")
 
     def choose_output_ps3(self):
         dir = filedialog.askdirectory()
-        if dir == "":
-            return
-        else:
+        if dir:
             self.output_ps3.append(dir)
-            lbl2.configure(text="Chosen PS3 save output path: " + dir)
-            print(f"DEBUG: chosen output directory: {self.output_ps3}")
+            self.ui.lbl2.configure(text="Chosen PS3 save output path: " + dir)
+            print(f"DEBUG: chosen output directory: {dir}")
 
     def choose_output_pc(self):
         dir = filedialog.askdirectory()
-        if dir == "":
-            return
-        else:
+        if dir:
             self.output_pc.append(dir)
-            lbl4.configure(text="Chosen PC save output path: " + dir)
-            print(f"DEBUG: chosen output directory: {self.output_pc}")
+            self.ui.lbl4.configure(text="Chosen PC save output path: " + dir)
+            print(f"DEBUG: chosen output directory: {dir}")
 
     def le_to_be(self):
         output_file_ps3 = os.path.join(*self.output_ps3, "OUTPUT_PC")
@@ -70,9 +52,9 @@ class File:
         with open(output_file_ps3, "wb") as f:
             f.write(converted_file.buffer())
         print(f"DEBUG: CONVERSION PS3 -> PC SUCCESS!!!")
-        messagebox.showinfo('Info','Conversion from PS3 to PC success!!')
+        messagebox.showinfo('Info', 'Conversion from PS3 to PC success!!')
 
-    def be_to_le(self):
+    def be_to_le(self): #still a WIP as main focus is PS3->PC conversion at this moment
         output_file_pc = os.path.join(*self.output_pc, "OUTPUT_PS3")
         input_file_pc = open(os.path.join(*self.input_pc), "rb")
         save2 = BinaryReader(input_file_pc.read(), True)
@@ -84,45 +66,18 @@ class File:
         with open(output_file_pc, "wb") as f:
             f.write(converted_file.buffer())
         print(f"DEBUG: CONVERSION PS3 <- PC SUCCESS!!!")
-        messagebox.showinfo('Info','Conversion from PC to PS3 success!!')
+        messagebox.showinfo('Info', 'Conversion from PC to PS3 success!!')
+
+    def close(self):
+        exit()
 
 
-filemanagement = File()
+def main():
+    file_manager = File(None)
+    main_app = ui.UserInterface(file_manager)
+    file_manager.ui = main_app
+    main_app.mainloop()
 
 
-window = Tk()
-window.geometry('600x600')
-window.title("Yakuzer save converter proto a0.2")
-bg = PhotoImage(file = "background.png")
-label1 = Label(window, image = bg)
-label1.place(x = 0, y = 0)
-
-menu = Menu(window)
-new_item = Menu(menu, tearoff=0)
-new_item.add_command(label='Open PC save...', command=filemanagement.choose_pc_input)
-new_item.add_command(label='Open PS3 save...', command=filemanagement.choose_ps3_input)
-new_item.add_command(label='Exit', command=close)
-menu.add_cascade(label='File', menu=new_item)
-window.config(menu=menu)
-
-lbl1 = Label(window, text="Selected PS3 save path goes here...") #ps3 save input path
-lbl1.pack()
-lbl2 = Label(window, text="Selected PC output path goes here...") #output path pc
-lbl2.pack(pady=5)
-lbl3 = Label(window, text="Selected PC save path goes here...") #pc save input path
-lbl3.pack(pady=10)
-lbl4 = Label(window, text="Selected PS3 output path goes here...") #output path ps3
-lbl4.pack(pady=15)
-btn1 = Button(window, text="Choose PS3 output directory", command=filemanagement.choose_output_ps3)
-btn1.pack(side="bottom", anchor = NW)
-btn4 = Button(window, text="Choose PC output directory", command=filemanagement.choose_output_pc)
-btn4.pack(side="bottom", anchor = NW)
-btn2 = Button(window, text="Convert PS3 -> PC!!!", command=filemanagement.le_to_be)
-btn2.pack(side="bottom", anchor = NW)
-btn3 = Button(window, text="Convert PC -> PS3!!!", command=filemanagement.be_to_le)
-btn3.pack(side="bottom", anchor = NW)
-
-btn5 = Button(window, text='Music???', command=play)
-btn5.pack(side="bottom", anchor = SW)
-
-window.mainloop()
+if __name__ == "__main__":
+    main()
